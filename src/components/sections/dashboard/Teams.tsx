7e04a6352ac7d4a522a754a5135c240d338e4bef
beyzanurakgul <beyzanurakgul@hotmail.com>
2024-05-24 10:16:21 +0300

    güncel kodlar

--- src/components/sections/dashboard/Teams.tsx
+++ src/components/sections/dashboard/Teams.tsx
@@ -1,9 +1,7 @@
-// Teams.tsx
 import React, { useState } from 'react';
 import GenericModal from '../../ui/GenericModal';
-import GenericList from '../../ui/GenericList';
-import GenericCard from '../../ui/GenericCard';
-import { useStore } from '../../../utils/useStore';
+import GenericList from '../../ui/GenericList'; // GenericList bileşenini içe aktar
+import GenericCard from '../../ui/GenericCard'; // GenericCard bileşenini içe aktar
 
 interface Employee {
   name: string;
@@ -27,9 +25,10 @@ interface TeamsProps {
 }
 
 const Teams: React.FC<TeamsProps> = ({ data }) => {
-  const { setData, setLoading, setError } = useStore();
   const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  if (!data) return <p className="text-red-500">No teams data available</p>;
 
   const openModal = (team: Team) => {
     setSelectedTeam(team);
@@ -43,7 +42,41 @@ const Teams: React.FC<TeamsProps> = ({ data }) => {
 
   const renderEmployeeItem = (employee: Employee) => (
     <div className="p-4">
-      {/* Employee item render işlevi */}
+      <div className="flex items-center mb-4">
+        <div className="relative w-12 h-12 overflow-hidden bg-indigo-100 rounded-full dark:bg-gray-200">
+          <svg
+            className="w-full h-full text-indigo-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <p className="text-gray-800 ml-4">
+          <span className="font-semibold"></span> {employee.name}
+        </p>
+      </div>
+
+      <p className="text-gray-800">
+        <span className="font-semibold">Title:</span> {employee.title}
+      </p>
+      <p className="text-gray-800">
+        <span className="font-semibold">Current Score:</span> {employee.current_score}
+      </p>
+      <p className="text-gray-800">
+        <span className="font-semibold">Email:</span> {employee.email}
+      </p>
+      <p className="text-gray-800">
+        <span className="font-semibold">Lessons Taken:</span> {employee.lessons_taken}
+      </p>
+      <p className="text-gray-800">
+        <span className="font-semibold">Skills Being Developed:</span> {employee.skills_being_developed.join(', ')}
+      </p>
     </div>
   );
 
@@ -53,8 +86,8 @@ const Teams: React.FC<TeamsProps> = ({ data }) => {
         <GenericList
           items={data}
           title="Teams"
-          renderItem={(team: Team) => (
-            <GenericCard onShowMore={() => openModal(team)}>
+          renderItem={(team) => (
+            <GenericCard onShowMore={() => openModal(team)}> {/* onShowMore prop'unu iletmek */}
               <div className="cursor-pointer">
                 <h3 className="text-xl font-bold mb-2 text-blue-700">{team.title}</h3>
                 <p className="text-gray-700 mb-2">{team.description}</p>
@@ -79,9 +112,9 @@ const Teams: React.FC<TeamsProps> = ({ data }) => {
             Total Employee Count: <span className="font-semibold">{selectedTeam.total_employee_count}</span>
           </p>
           <h4 className="font-bold text-xl text-gray-900 mb-2">Employees:</h4>
-          <ul>
+          <ul >
             {selectedTeam.employees.map((employee, idx) => (
-              <li key={idx} className="ml-4">
+              <li key={idx} className="ml-4 font-bold">
                 {renderEmployeeItem(employee)}
               </li>
             ))}
